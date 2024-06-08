819ca21ea44796799735ff5ccdcd3583552d6c38
Erik3421 <136456102+Erik3421@users.noreply.github.com>
2024-06-08 09:15:02 +0400

    Update main.js

--- main.js
+++ main.js
@@ -27,185 +27,198 @@ const COIN_FALL_SPEED = 40;
 
 setGravity(640);
 
-const player = add([
-    sprite("bean"),
+const tapToStart = add([
+    text("Tap to Start", { size: 48, font: "RetroGaming" }),
     pos(center()),
     anchor("center"),
-    area(),
-    body(),
-    scale(1.5),
 ]);
 
-player.play("idle");
+onTouchStart(() => {
+    destroy(tapToStart);
+    startGame();
+});
 
-const ground = add([
-    rect(width(), 36),
-    area(),
-    outline(1),
-    pos(0, height() - 36),
-    body({ isStatic: true }),
-]);
+function startGame() {
+    const player = add([
+        sprite("bean"),
+        pos(center()),
+        anchor("center"),
+        area(),
+        body(),
+        scale(1.5),
+    ]);
 
-function updatePlayerAnimation() {
-    if (player.isGrounded()) {
-        if (isTouchingLeft || isKeyDown("left") || isTouchingRight || isKeyDown("right")) {
-            player.play("run");
-        } else {
-            player.play("idle");
+    player.play("idle");
+
+    const ground = add([
+        rect(width(), 36),
+        area(),
+        outline(1),
+        pos(0, height() - 36),
+        body({ isStatic: true }),
+    ]);
+
+    function updatePlayerAnimation() {
+        if (player.isGrounded()) {
+            if (isTouchingLeft || isKeyDown("left") || isTouchingRight || isKeyDown("right")) {
+                player.play("run");
+            } else {
+                player.play("idle");
+            }
         }
     }
-}
 
-player.onGround(() => {
-    updatePlayerAnimation();
-});
-
-onKeyPress("space", () => {
-    if (player.isGrounded()) {
-        player.jump(JUMP_FORCE);
-        player.play("jump");
-    }
-});
-
-onKeyDown("left", () => {
-    isTouchingLeft = true;
-    player.move(-SPEED, 0);
-    player.flipX = true;
-    if (player.isGrounded() && player.curAnim() !== "run") {
-        player.play("run");
-    }
-});
-
-onKeyDown("right", () => {
-    isTouchingRight = true;
-    player.move(SPEED, 0);
-    player.flipX = false;
-    if (player.isGrounded() && player.curAnim() !== "run") {
-        player.play("run");
-    }
-});
-
-["left", "right"].forEach((key) => {
-    onKeyRelease(key, () => {
-        if (key === "left") {
-            isTouchingLeft = false;
-        }
-        if (key === "right") {
-            isTouchingRight = false;
-        }
+    player.onGround(() => {
         updatePlayerAnimation();
     });
-});
 
-let score = 0;
-
-const scoreLabel = add([
-    text(`Score: ${score}`, { size: 24, font: "RetroGaming" }),
-    pos(24, 24),
-]);
-
-function updateScore(points) {
-    score += points;
-    scoreLabel.text = `Score: ${score}`;
-
-    const plus100 = add([
-        text("+100", { size: 18, font: "RetroGaming" }),
-        pos(player.pos),
-        color(255, 232, 94),
-    ]);
-
-    plus100.onUpdate(() => {
-        plus100.move(0, -20);
-        plus100.opacity -= 0.05;
-        if (plus100.opacity <= 0) {
-            destroy(plus100);
-        }
-    });
-}
-
-function spawnCoin() {
-    const coin = add([
-        sprite("coin"),
-        pos(rand(0, width()), 0),
-        area(),
-        scale(0.75),
-        "coin",
-    ]);
-
-    coin.play("spin");
-
-    coin.onUpdate(() => {
-        coin.move(0, COIN_FALL_SPEED);
-        if (coin.pos.y > height()) {
-            destroy(coin);
+    onKeyPress("space", () => {
+        if (player.isGrounded()) {
+            player.jump(JUMP_FORCE);
+            player.play("jump");
         }
     });
 
-    wait(rand(1, 3), spawnCoin);
-}
-
-spawnCoin();
-
-player.onCollide("coin", (coin) => {
-    destroy(coin);
-    updateScore(100);
-});
-
-player.onUpdate(() => {
-    if (player.pos.x < 0) player.pos.x = 0;
-    if (player.pos.x > width()) player.pos.x = width();
-});
-
-// Обработка касаний для мобильного управления
-let isTouchingLeft = false;
-let isTouchingRight = false;
-
-onTouchStart((pos, t) => {
-    if (pos.x < width() / 2) {
+    onKeyDown("left", () => {
         isTouchingLeft = true;
-        isTouchingRight = false;
         player.move(-SPEED, 0);
         player.flipX = true;
         if (player.isGrounded() && player.curAnim() !== "run") {
             player.play("run");
         }
-    } else {
+    });
+
+    onKeyDown("right", () => {
         isTouchingRight = true;
+        player.move(SPEED, 0);
+        player.flipX = false;
+        if (player.isGrounded() && player.curAnim() !== "run") {
+            player.play("run");
+        }
+    });
+
+    ["left", "right"].forEach((key) => {
+        onKeyRelease(key, () => {
+            if (key === "left") {
+                isTouchingLeft = false;
+            }
+            if (key === "right") {
+                isTouchingRight = false;
+            }
+            updatePlayerAnimation();
+        });
+    });
+
+    let score = 0;
+
+    const scoreLabel = add([
+        text(`Score: ${score}`, { size: 24, font: "RetroGaming" }),
+        pos(24, 24),
+    ]);
+
+    function updateScore(points) {
+        score += points;
+        scoreLabel.text = `Score: ${score}`;
+
+        const plus100 = add([
+            text("+100", { size: 18, font: "RetroGaming" }),
+            pos(player.pos),
+            color(255, 232, 94),
+        ]);
+
+        plus100.onUpdate(() => {
+            plus100.move(0, -20);
+            plus100.opacity -= 0.05;
+            if (plus100.opacity <= 0) {
+                destroy(plus100);
+            }
+        });
+    }
+
+    function spawnCoin() {
+        const coin = add([
+            sprite("coin"),
+            pos(rand(0, width()), 0),
+            area(),
+            scale(0.75),
+            "coin",
+        ]);
+
+        coin.play("spin");
+
+        coin.onUpdate(() => {
+            coin.move(0, COIN_FALL_SPEED);
+            if (coin.pos.y > height()) {
+                destroy(coin);
+            }
+        });
+
+        wait(rand(1, 3), spawnCoin);
+    }
+
+    spawnCoin();
+
+    player.onCollide("coin", (coin) => {
+        destroy(coin);
+        updateScore(100);
+    });
+
+    player.onUpdate(() => {
+        if (player.pos.x < 0) player.pos.x = 0;
+        if (player.pos.x > width()) player.pos.x = width();
+    });
+
+    // Обработка касаний для мобильного управления
+    let isTouchingLeft = false;
+    let isTouchingRight = false;
+
+    onTouchStart((pos, t) => {
+        if (pos.x < width() / 2) {
+            isTouchingLeft = true;
+            isTouchingRight = false;
+            player.move(-SPEED, 0);
+            player.flipX = true;
+            if (player.isGrounded() && player.curAnim() !== "run") {
+                player.play("run");
+            }
+        } else {
+            isTouchingRight = true;
+            isTouchingLeft = false;
+            player.move(SPEED, 0);
+            player.flipX = false;
+            if (player.isGrounded() && player.curAnim() !== "run") {
+                player.play("run");
+            }
+        }
+    });
+
+    onTouchEnd((pos, t) => {
         isTouchingLeft = false;
-        player.move(SPEED, 0);
-        player.flipX = false;
-        if (player.isGrounded() && player.curAnim() !== "run") {
-            player.play("run");
-        }
-    }
-});
+        isTouchingRight = false;
+        updatePlayerAnimation();
+    });
 
-onTouchEnd((pos, t) => {
-    isTouchingLeft = false;
-    isTouchingRight = false;
-    updatePlayerAnimation();
-});
-
-onTouchMove((pos, t) => {
-    if (pos.y < height() / 2 && player.isGrounded()) {
-        player.jump(JUMP_FORCE);
-        player.play("jump");
-    }
-});
-
-player.onUpdate(() => {
-    if (isTouchingLeft) {
-        player.move(-SPEED, 0);
-        player.flipX = true;
-        if (player.isGrounded() && player.curAnim() !== "run") {
-            player.play("run");
+    onTouchMove((pos, t) => {
+        if (pos.y < height() / 2 && player.isGrounded()) {
+            player.jump(JUMP_FORCE);
+            player.play("jump");
         }
-    }
-    if (isTouchingRight) {
-        player.move(SPEED, 0);
-        player.flipX = false;
-        if (player.isGrounded() && player.curAnim() !== "run") {
-            player.play("run");
+    });
+
+    player.onUpdate(() => {
+        if (isTouchingLeft) {
+            player.move(-SPEED, 0);
+            player.flipX = true;
+            if (player.isGrounded() && player.curAnim() !== "run") {
+                player.play("run");
+            }
         }
-    }
-});
+        if (isTouchingRight) {
+            player.move(SPEED, 0);
+            player.flipX = false;
+            if (player.isGrounded() && player.curAnim() !== "run") {
+                player.play("run");
+            }
+        }
+    });
+}
